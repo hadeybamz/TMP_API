f0f5d3cf2e563e0e36a225ad978159cb9fef129a
Hadeybamz <adeyemi.bamgbla@gmail.com>
2023-02-09 03:22:52 +0100

    Add DataContext

---
+++ TMP_API/Context/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMP_API.Models.Customers;
+using TMP_API.Models.Orders;
+using TMP_API.Models.Products;
+
+namespace TMP_API.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(){}
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        private readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to SqlServer database
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
